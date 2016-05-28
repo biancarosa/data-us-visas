f5a08f49a22de6bc2982ce090a8024c994d71d43
biancarosa <me@biancarosa.com.br>
2016-05-28 12:56:00 -0300

    Fix markdown and add graph to script steps

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Introduction
 
-This is a analysis that uses data from the (United States Department of Labor that contains information about foreign labor)[https://www.foreignlaborcert.doleta.gov/performancedata.cfm] certification requests. We use (disclosure data)[https://www.foreignlaborcert.doleta.gov/docs/Performance_Data/Disclosure/FY16Q2/PERM_Disclosure_Data_FY16_Q2.xlsx] that cover determinations issued between October 1, 2015 through March 31, 2016.
+This is a analysis that uses data from the [United States Department of Labor that contains information about foreign labor](https://www.foreignlaborcert.doleta.gov/performancedata.cfm) certification requests. We use [disclosure data](https://www.foreignlaborcert.doleta.gov/docs/Performance_Data/Disclosure/FY16Q2/PERM_Disclosure_Data_FY16_Q2.xlsx) that cover determinations issued between October 1, 2015 through March 31, 2016.
 
 # The script
 1. Substituite column names because *rows* doesn't support column names starting with numbers.
@@ -10,3 +10,4 @@ This is a analysis that uses data from the (United States Department of Labor th
 5. Print number of requests from developers that were certified vs requests from developers that were denied
 6. Print number of requests from brazilians vs requests from non-brazilians
 7. Print number of requests from brazilians that were certified vs requests from brazilians that were denied
+8. Generates a horizontal bar plot with number of certified request by country
--- perm-visa.py
+++ perm-visa.py
@@ -1,9 +1,5 @@
 import rows
 from io import BytesIO
-#import requests
-#url = 'https://www.foreignlaborcert.doleta.gov/docs/Performance_Data/Disclosure/FY16Q2/PERM_Disclosure_Data_FY16_Q2.xlsx'
-#xlsx = requests.get(url).content  # Download XLSX data
-#visas_data = rows.import_from_xlsx(BytesIO(xlsx))
 
 print '------\nReading file\n-----'
 filename = 'data/PERM_Disclosure_Data_FY16_Q2_shorter.csv' # shorter csv
@@ -29,16 +25,20 @@ developers = filter(lambda row: row.pw_soc_code == developer_code,  visas_data)
 non_developers = filter(lambda row: row.pw_soc_code != developer_code,  visas_data)
 print 'Devs vs Non-Devs: {} vs {}'.format(len(developers), len(non_developers))
 
-developers_certified = filter(lambda row: row.case_status == 'Certified' and row.pw_soc_code == developer_code,  visas_data)
-developers_denied = filter(lambda row: row.case_status == 'Denied' and row.pw_soc_code != developer_code,  visas_data)
+developers_certified = filter(lambda row: row.case_status == 'Certified'
+                                and row.pw_soc_code == developer_code,  visas_data)
+developers_denied = filter(lambda row: row.case_status == 'Denied'
+                                and row.pw_soc_code != developer_code,  visas_data)
 print 'Devs Certified vs Devs Denied: {} vs {}'.format(len(developers_certified), len(developers_denied))
 
 brazil = filter(lambda row: row.country_of_citizenship == 'BRAZIL', visas_data)
 other_countries = filter(lambda row: row.country_of_citizenship != 'BRAZIL', visas_data)
 print 'Brazil vs Other countries: {} vs {}'.format(len(brazil), len(other_countries))
 
-brazil_certified = filter(lambda row: row.case_status == 'Certified' and  row.country_of_citizenship == 'BRAZIL',  visas_data)
-brazil_denied = filter(lambda row: row.case_status == 'Denied' and row.country_of_citizenship != 'BRAZIL',  visas_data)
+brazil_certified = filter(lambda row: row.case_status == 'Certified'
+                                and  row.country_of_citizenship == 'BRAZIL',  visas_data)
+brazil_denied = filter(lambda row: row.case_status == 'Denied'
+                                    and row.country_of_citizenship != 'BRAZIL',  visas_data)
 print 'Brazil Certified vs Brazil Denied: {} vs {}'.format(len(brazil_certified), len(brazil_denied))
 
 import matplotlib.pyplot as plt
